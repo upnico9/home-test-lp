835020cfe333bcf81bec1145e5376ddffa801344
upnico <nicolas.leroux.1@proton.me>
2025-10-21 20:20:11 +0200

    update index.js to add comparison with old outputs, and add output with dafalgan file

--- index.js
+++ index.js
@@ -1,31 +1,68 @@
 import { Drug, Pharmacy } from "./src/index.js";
 import fs from "fs";
 
-const drugs = [
+function compareOutputs(file1, file2) {
+  try {
+    const content1 = fs.readFileSync(file1, 'utf8');
+    const content2 = fs.readFileSync(file2, 'utf8');
+    return content1 === content2;
+  } catch (error) {
+    return false;
+  }
+}
+
+const originalDrugs = [
   new Drug("Doliprane", 20, 30),
   new Drug("Herbal Tea", 10, 5),
   new Drug("Fervex", 12, 35),
   new Drug("Magic Pill", 15, 40),
 ];
-const pharmacy = new Pharmacy(drugs);
+const originalPharmacy = new Pharmacy(originalDrugs);
 
-const log = [];
+const originalLog = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.parse(JSON.stringify(pharmacy.updateBenefitValue())));
+  originalLog.push(JSON.parse(JSON.stringify(originalPharmacy.updateBenefitValue())));
 }
 
-/* eslint-disable no-console */
+fs.writeFileSync(
+  "output_verification.json",
+  JSON.stringify({ result: originalLog }, null, 2).concat("\n")
+);
+
+const isIntegrityOk = compareOutputs("output.json", "output_verification.json");
+
+if (isIntegrityOk) {
+  console.log("Congrats ! You didn't break anything");
+} else {
+  console.log("Not cool, you broke the pharmacy");
+}
+
+const newDrugs = [
+  new Drug("Doliprane", 20, 30),
+  new Drug("Herbal Tea", 10, 5),
+  new Drug("Fervex", 12, 35),
+  new Drug("Magic Pill", 15, 40),
+  new Drug("Dafalgan", 15, 30),
+];
+const newPharmacy = new Pharmacy(newDrugs);
+
+const newLog = [];
+
+for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
+  newLog.push(JSON.parse(JSON.stringify(newPharmacy.updateBenefitValue())));
+}
+
 fs.writeFile(
-  "output.json",
-  JSON.stringify({ result: log }, null, 2).concat("\n"),
+  "output_with_dafalgan.json",
+  JSON.stringify({ result: newLog }, null, 2).concat("\n"),
   (err) => {
     if (err) {
-      console.log("error");
+      console.log("Error creating new file for dafalgan output");
     } else {
-      console.log("success");
+      console.log("output_with_dafalgan.json created !");
     }
   },
 );
 
-/* eslint-enable no-console */
+fs.unlinkSync("output_verification.json");
