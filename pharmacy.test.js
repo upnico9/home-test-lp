07a4f40837cc68ef8ebca392b33f689d02349240
upnico <nicolas.leroux.1@proton.me>
2025-10-21 18:46:33 +0200

    Add tests for each type of drugs

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,85 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)],
-    );
+  test("should handle basic drugs", () => {
+    const drug = new Drug("test", 2, 3);
+    const pharmacy = new Pharmacy([drug]);
+
+    pharmacy.updateBenefitValue();
+
+    expect(drug.expiresIn).toBe(1);
+    expect(drug.benefit).toBe(2);
+  });
+
+
+  test("should handle Herbal Tea", () => {
+    const drug = new Drug('Herbal Tea', 10, 5);
+    const pharmacy = new Pharmacy([drug]);
+
+    pharmacy.updateBenefitValue();
+
+    expect(drug.expiresIn).toBe(9);
+    expect(drug.benefit).toBe(6);
+  });
+
+  test("should handle Herbal Tea expired", () => {
+    const drug = new Drug('Herbal Tea', 0, 5);
+    const pharmacy = new Pharmacy([drug]);
+
+    pharmacy.updateBenefitValue();
+
+    expect(drug.expiresIn).toBe(-1);
+    expect(drug.benefit).toBe(7);
+  });
+
+  test("should handle Magic Pill", () => {
+    const drug = new Drug("Magic Pill", 15, 40);
+    const pharmacy = new Pharmacy([drug]);
+
+    pharmacy.updateBenefitValue();
+
+    expect(drug.expiresIn).toBe(15);
+    expect(drug.benefit).toBe(40);
+  });
+
+
+  test("should handle Fervex", () => {
+    const drug = new Drug("Fervex", 15, 20);
+    const pharmacy = new Pharmacy([drug]);
+
+    pharmacy.updateBenefitValue();
+
+    expect(drug.expiresIn).toBe(14);
+    expect(drug.benefit).toBe(21);
+  });
+
+  test("should handle Fervex 10 days", () => {
+    const drug = new Drug("Fervex", 10, 20);
+    const pharmacy = new Pharmacy([drug]);
+
+    pharmacy.updateBenefitValue();
+
+    expect(drug.expiresIn).toBe(9);
+    expect(drug.benefit).toBe(22);
+  });
+
+  test("should handle Fervex 5 days", () => {
+    const drug = new Drug("Fervex", 5, 30);
+    const pharmacy = new Pharmacy([drug]);
+
+    pharmacy.updateBenefitValue();
+
+    expect(drug.expiresIn).toBe(4);
+    expect(drug.benefit).toBe(33);
+  });
+
+  test("should handle Fervex expired", () => {
+    const drug = new Drug("Fervex", 0, 30);
+    const pharmacy = new Pharmacy([drug]);
+
+    pharmacy.updateBenefitValue();
+
+    expect(drug.expiresIn).toBe(-1);
+    expect(drug.benefit).toBe(0);
   });
 });
